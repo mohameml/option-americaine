9e2ba7ab819ec0d304cb2f2054dd120983afb27f
mohameml <mhmdahmdnbyl18@gmail.com>
2024-10-27 23:09:23 +0100

    Projet Option-Americain :init

---
+++ build/CMakeFiles/test_json_reader.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_json_reader.dir/src/json_helper.cpp.o"
+  "CMakeFiles/test_json_reader.dir/src/json_helper.cpp.o.d"
+  "CMakeFiles/test_json_reader.dir/src/test_json_reader.cpp.o"
+  "CMakeFiles/test_json_reader.dir/src/test_json_reader.cpp.o.d"
+  "test_json_reader"
+  "test_json_reader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_json_reader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
